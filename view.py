4a4ba1a9fc3f6768262a99ae5e4e68c62799d01f
Georgvit <ifreyd@mail.ru>
2022-10-24 22:59:45 +0300

    исправление ошибок 2

--- view.py
+++ view.py
@@ -1,6 +1,7 @@
 from tabulate import tabulate
 
 # Визуализация программы
+# Первый пльзователь
 def hello_one():
     # Приветствуем игроков
     print('Приветствую вас в игре крестики нолики!')
@@ -8,26 +9,22 @@ def hello_one():
     print(f'Приветствую тебя, {name_one_player}!')
     return name_one_player
 
-
+# Второй пльзователь
 def hello_two():
     name_two_player = input("Введите имя второго игрока: ")
     print(f'Приветствую тебя, {name_two_player}!')
     print()
     return name_two_player
 
-
+# Создание игрового поля
 def playing_field():
-    # Создание игрового поля
     matrix_game = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
     headers_table = ['GAME ', 'for ', 'YOU ']
-
     print(tabulate(matrix_game, headers=headers_table, tablefmt='fancy_grid'))
-
     return matrix_game
 
-
+# Вывод победителя
 def winner(count, random_move, name_one_player, name_two_player):
-    # Вывод победителя
     if count == True and random_move == 2:
         print(f'🔥🔥🔥 Победил игрок: {name_one_player} 🔥🔥🔥')
     elif count == True and random_move == 1:
